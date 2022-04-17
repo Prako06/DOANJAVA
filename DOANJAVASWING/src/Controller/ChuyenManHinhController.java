ab5a3816a5ac9f7254e470227a9c296212220efb
Prako06 <anhkhoiphamtran21@gmail.com>
2022-04-17 23:21:38 +0700

    Giao dien

---
+++ DOANJAVASWING/src/Controller/ChuyenManHinhController.java
@@ -0,0 +1,131 @@
+package Controller;
+
+import Bean.DanhMucBean;
+import View.BanHangJPanel;
+import View.HangHoaJPanel;
+import View.KhachHangJPanel;
+import View.NhanVienPanel;
+import View.ThongKeJPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author PraKo
+ */
+public class ChuyenManHinhController {
+    private JPanel root;
+    private String kindSelected = "";
+    
+    private List<DanhMucBean> listItem = null;
+
+    public ChuyenManHinhController(JPanel jpnRoot) {
+        this.root = jpnRoot;
+    }
+    
+    public void setView(JPanel jpnItem, JLabel jlbItem){
+        kindSelected = "KhachHang";
+        jpnItem.setBackground(new Color(0,40,255));
+        jlbItem.setBackground(new Color(0,40,255));
+        
+        root.removeAll();
+        root.setLayout(new BorderLayout());
+        root.add(new KhachHangJPanel());
+        root.validate();
+        root.repaint();
+    }
+    
+    public void setEvent(List<DanhMucBean> listItem){
+        this.listItem = listItem;
+        for(DanhMucBean item : listItem){
+            item.getJlb().addMouseListener(new LabelEvent(item.getKind(), item.getJpn(), item.getJlb()));
+        }
+    }
+    
+    class LabelEvent implements MouseListener{
+        
+        private JPanel node;
+        private String kind;
+        
+        
+        private JPanel jpnItem;
+        private JLabel jlbItem;
+
+        public LabelEvent(String kind, JPanel jpnItem, JLabel jlbItem) {
+            this.kind = kind;
+            this.jpnItem = jpnItem;
+            this.jlbItem = jlbItem;
+        }
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {      
+            switch(kind){
+                case "KhachHang":
+                    node = new KhachHangJPanel();
+                    break;
+                case "ThongKe":
+                    node = new ThongKeJPanel();
+                    break;
+                case "HangHoa":
+                    node = new HangHoaJPanel();
+                    break;
+                case "NhanVien":
+                    node = new NhanVienPanel();
+                    break;
+                case "BanHang":
+                    node = new BanHangJPanel();
+                    break;
+                default:
+                    break;
+            }
+            root.removeAll();
+            root.setLayout(new BorderLayout());
+            root.add(node);
+            root.validate();
+            root.repaint();
+            setChangeBackground(kind);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            kindSelected = kind;
+            jpnItem.setBackground(new Color(0,40,255));
+            jlbItem.setBackground(new Color(0,40,255));      
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            jpnItem.setBackground(new Color(0,40,255));
+            jlbItem.setBackground(new Color(0,40,255)); 
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            if(kindSelected.equalsIgnoreCase(kind)){
+                jpnItem.setBackground(new Color(0,104,255));
+                jlbItem.setBackground(new Color(0,104,255));
+            }
+        }            
+    }
+    
+    private void setChangeBackground(String kind){
+        for(DanhMucBean item : listItem){
+            if(item.getKind().equalsIgnoreCase(kind)){
+                item.getJpn().setBackground(new Color(0,40,255));
+                item.getJlb().setBackground(new Color(0,40,255));
+            }else{
+                item.getJpn().setBackground(new Color(0,104,255));
+                item.getJlb().setBackground(new Color(0,104,255));
+            }
+        }
+    }
+}
